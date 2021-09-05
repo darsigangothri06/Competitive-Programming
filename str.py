f49c8935f7a399f9a92fec6102a5931786daeca0
darsigangothri06 <darsigangothri@gmail.com>
2021-09-05 18:03:55 +0530

    deleted

--- str.py
+++ str.py
@@ -22,45 +22,7 @@ for i in L:
         Q.append(int(i))
 print(','.join(map(str,P)))
 
-from itertools import permutations
-Str = input()
-Str2 = input()
-L = [''.join(p) for p in permutations(Str)]
-if Str2 in L:
-    print('true')
-else:
-    print('false')
 '''
-from statistics import median
-L = input().split(',')
-L = list(map(int,L))
-K = int(L[0])
-L.remove(L[0])
-print(L)
-Q = []
-for i in range(1,K):
-    j = L[:i]
-    Q.append(int(median(j)))
-    '''
-A = [L[0]]
-B = [L[0],L[1]]
-Q = []
-Q.extend([int(median(A)),int(median(B))])
-
-if len(L) % 2 != 0:
-    print('od')
-    '''
-P = [int(median(L[i:i+K])) for i in range(len(L)-1)]
-Q.extend(P)
-print(Q)
-'''
-else:
-    print('evee')
-    P = [int(median(L[i:i+K])) for i in range(len(L))]
-    Q.extend(P)
-    print(Q)
-    '''
-
 
 
 
