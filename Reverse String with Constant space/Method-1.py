0d8b0806632aa3eee22c81ddc47d7969a84dc5b9
darsigangothri06 <darsigangothri@gmail.com>
2021-09-06 20:14:01 +0530

    solved reverse string

--- Reverse String with Constant space/Method-1.py
+++
@@ -1,3 +0,0 @@
-S = input("Enter comma-separated characters: ").split(',')
-k = ''.join(S)
-print(list(k[::-1]))
--- armstrong.py
+++
@@ -1,5 +0,0 @@
-num = input("Enter a number: ")
-Sum = 0
-for i in num:
-    Sum += int(i)**3
-print('true') if int(num) == Sum else print('false')
