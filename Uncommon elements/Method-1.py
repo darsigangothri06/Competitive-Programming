b1699859226a4d7561dc517b14777ccca96b9890
darsigangothri06 <darsigangothri@gmail.com>
2021-09-08 08:33:14 +0530

    initial commit

--- Uncommon elements/Method-1.py
+++ Uncommon elements/Method-1.py
@@ -1,9 +1,5 @@
-L1 = [1,1,2,3,4,5,5,7,6,9,10]
-L2 = [11,12,13,45,6,7,18,19,20]
-Emp = L1 + L2
-cnt = 0
-for i in Emp:
-    if i not in L1 or i not in L2:
-        cnt += 1
-        print(i)
-print(cnt)
+L1 = list(map(int,input("Enter space separated list1: ").split()))
+L2 = list(map(int,input("Enter space separated list2: ").split()))
+L3 = [i for i in L2 if i not in L1]
+New = [i for i in L1 if i not in L2]
+print(len(New)+len(L3))
--- chk.py
+++
@@ -1,4 +0,0 @@
-i = 2
-print(2*(i+1))
-for j in range(1,2(i+1)):
-    print(j)
