ff3860fe52d0c11be75e54625214ddf8404e9758
darsigangothri06 <darsigangothri@gmail.com>
2021-09-08 08:34:02 +0530

    initial commit

--- Uncommon elements/Method-1.py
+++
@@ -1,5 +0,0 @@
-L1 = list(map(int,input("Enter space separated list1: ").split()))
-L2 = list(map(int,input("Enter space separated list2: ").split()))
-L3 = [i for i in L2 if i not in L1]
-New = [i for i in L1 if i not in L2]
-print(len(New)+len(L3))
